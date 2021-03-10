c759eea712f6a9469049232d9b1fe361d5bb2cb8
mnajmi007 <80237630+mnajmi007@users.noreply.github.com>
2021-03-10 21:43:16 +0700

    Permutation and Subtitutions Cipher

---
+++ Permutation.py
@@ -0,0 +1,28 @@
+# from itertools import permutations
+#
+# my_list = [1, 2, 3, 4, 5, 6, 7]
+# list_of_permutations = permutations(my_list)
+# cnt = 0
+# for permutation in list_of_permutations:
+#     cnt += 1
+# print(len(my_list), cnt)
+
+import cProfile
+
+def faculty(n):
+    if n <= 1:
+        return n
+    else:
+        return faculty(n-1)*n
+
+def counter(n):
+    cnt = 0
+    for i in range(n):
+        cnt+=1
+    return cnt
+
+cProfile.run("counter(faculty(13))")
+
+# for i in range(10):
+#     print(faculty(i))
+
---
+++ SubtitutionsCipher.py
@@ -0,0 +1,34 @@
+import random
+
+def generate_key():
+    letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cletters = list(letters)
+    key={}
+
+    for c in letters:
+        key[c] = cletters.pop(random.randint(0, len(cletters) - 1))
+    return key
+
+def encrypt(key, message):
+    cipher=""
+    for c in message:
+        if c in key:
+            cipher += key[c]
+        else:
+            cipher += c
+    return cipher
+
+def get_decrypt_key(key):
+    dkey={}
+    for k in key:
+        dkey[key[k]] = k
+    return dkey
+
+message = "MUHAMMAD NAJMI"
+key = generate_key()
+cipher = encrypt(key, message)
+dkey = get_decrypt_key(key)
+decrypt = encrypt(dkey, cipher)
+
+print(cipher)
+print(decrypt)
